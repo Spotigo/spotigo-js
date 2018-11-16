02512be165f211341f26ccf2d4dc073de7fd9e65
bennyman123abc <bennyman123abc@hotmail.com>
2018-11-15 22:23:02 -0500

    Begin working on track objects

---
+++ src/SpotigoTrackInfo.ts
@@ -0,0 +1,11 @@
+export class SpotigoTrackInfo {
+
+    /**
+     * Represents the basic track info provided by Spotigo
+     * @constructor
+     * @see Track
+     */
+    constructor() {
+        console.log("unimplemented");
+    }
+}
