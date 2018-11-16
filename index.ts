da7d1973ba068fac7ed5d25d75f3a6d592912d2b
bennyman123abc <bennyman123abc@hotmail.com>
2018-11-16 16:37:12 -0500

    Add on the track stuff and a bit of client

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { Client } from "./src/Client";
+import { SpotigoTrackInfo } from "./src/SpotigoTrackInfo";
+
+module.exports.Client = Client;
+module.exports.SpotigoTrackInfo = SpotigoTrackInfo;
--- src/Client.ts
+++ src/Client.ts
@@ -1,8 +1,16 @@
+/**
+ * The client object that interacts with the Spotigo Web API
+ */
 export class Client {
-
     host: string;
     pass: string;
 
+    /**
+     * The client object that interacts with the Spotigo Web API
+     * @constructor
+     * @param host The URL to the Spotigo Web server you'd like to read from
+     * @param pass The password to access the data on the Spotigo Web server you'd like to read from (Leave blank for no password)
+     */
     constructor(host: string, pass: string = "") {
         this.host = host;
         this.pass = pass;
