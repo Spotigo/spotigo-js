60a80110c69d7eb9dc2b65552293432dfd1ba7d4
bennyman123abc <bennyman123abc@hotmail.com>
2018-11-16 16:37:38 -0500

    Expand on the first test (Will fail on the first commit)

--- travis_test.js
+++ travis_test.js
@@ -1,4 +1,21 @@
 // a file that will contain the travis ci tests
 // currently returns 0 to pass the tests
 
+const Spotigo = require("./index");
+
+var client = new Spotigo.Client(process.env.SPOTIGO_HOST, process.env.SPOTIGO_PASS);
+function assert(condition, message) {
+    if (!condition) {
+        throw new AssertException(message);
+    }
+}
+class AssertException {
+    constructor(message) {
+        this.name = 'AssertionError';
+        this.message = message || "Assert failed.";
+    }
+}
+
+assert(client.host != undefined, "Client failed to initialize");
+console.log(process.env.SPOTIGO_HOST);
 process.exit(0);
